1ac0d92d0d3cbfd88b5896e946e48e7e3973bf43
Soft Well House <bialonj@gmail.com>
2017-10-14 00:05:48 +0200

    pobranie danych i wyswietlenie

--- app/Http/Controllers/Site.php
+++ app/Http/Controllers/Site.php
@@ -3,7 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Contracts\Cache\Repository;
 
 class SiteController extends Controller {
-    
+
+    const _apikey = 'e8c15c7fa36020334f7bd9182b79d23c';
+
+    /**
+     * Wyświetlamy dane
+     * @return type
+     */
+    public function index() {
+// sprawdzamy czy istnieje w cachu klucz 'data' jeśli nie, to pobieramy dane z Api i dodajemy je do kesza
+        if (!Cache::has('data')) {
+            //pobiearamy dane dla warszawy
+            $data = $this->getWheter('Warszawa');
+            Cache::add('data', $data, 10);
+        }else{
+            // pobieramy dane z kesza
+            $data = Cache::get('data');
+        }
+        return view('weather', ['data' => $data]);
+    }
+
+    /**
+     * Pobieramy dane z Api OpenWeatherMap
+     * @param string $city_name nazwa miasta
+     * @return array
+     */
+    public function getWheter($city_name = 'Warszawa') {
+        // otwieramy plik json-a
+        return json_decode(file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=' . ucfirst($city_name) . '&appid=' . self::_apikey));
+    }
+
 }
--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -1,6 +1,61 @@
-<div class="weather-site">
-    <div class="col-md-12">
-        
-    </div>
-     
-</div>
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Pogoda - Warszawa</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <script src="/weather/public/js/app.js"></script>
+        <link href="/weather/public/css/app.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="weather-site">
+            <div class="col-md-12">
+                <div class="row">
+                    <section class="weather">
+                        <div class="col-md-5 col-md-offset-5 col-sm-6 col-sm-offset-5 box">
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3>Pogoda dla Warszawy</h3>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="row">
+                                        <div class="col-md-6 col-xs-6">
+                                            Lon: <?= $data->coord->lon ?>
+                                        </div>
+                                        <div class="col-md-6 col-xs-6">
+                                            Lat: <?= $data->coord->lat ?>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <img src="https://openweathermap.org/img/w/<?= $data->weather['0']->icon ?>.png" /> <?= $data->weather['0']->description ?>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="row">
+                                                <div class="col-md-12 col-xs-6">
+                                                    <h4>Temperatura</h4>
+                                                    <?= $data->main->humidity ?> &#186 F <br />
+                                                    <?= round(5 / 9 * ($data->main->humidity - 32), 1) ?> &#186 C
+                                                </div>
+                                                <div class="col-md-12 col-xs-6">
+                                                    <h4>Wiatr</h4>
+                                                    <?= $data->wind->speed ?> ms
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
